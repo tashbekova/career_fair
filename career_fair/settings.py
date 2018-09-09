5d5e7fb10693b330be71aa4bd6f90c4d71ad26f5
Tashbekova <tashbekovae@gmail.com>
2018-09-09 07:18:43 +0600

    edit

--- career_fair/settings.py
+++ career_fair/settings.py
@@ -129,3 +129,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+    )
+}
