343366b4a6fb279a5d80375bd0c67b83934df48a
Tashbekova <tashbekovae@gmail.com>
2018-09-11 19:07:13 +0600

    edit company_type

--- info/models.py
+++ info/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class CompanyType(models.Model):
-    type_name = models.CharField(max_length=100)
+    type_name = models.CharField(max_length=100,verbose_name="Вид партнерства")
 
     def __str__(self):
         return self.type_name
